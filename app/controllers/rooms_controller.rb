1c9dcc479b9eff78cebba438848181f1daf3c925
namaba <kensuke.namba@msetsu.com>
2020-10-28 16:45:38 +0900

    feat: ユーザー選択時roomがなければ作成

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,4 +1,7 @@
 class RoomsController < ApplicationController
+  before_action :set_user, only: %i[create]
+  before_action :has_room?, only: %i[create]
+
   def index
     @rooms = Room.all.order(:id)
   end
@@ -9,8 +12,22 @@ class RoomsController < ApplicationController
   end
 
   def create
-    @room = Room.build()
-    # @room.room_users.build(user: current_user)
-    # @room.room_users.build(user: params[:user_id])
+    @room = Room.new
+    if @room.create_with_users_rooms(current_user, @user)
+      redirect_to room_path(@room), notice: 'チャットルームが作成されました'
+    else
+      redirect_to root_path, alert: 'チャットルームの作成に失敗しました'
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find params[:user_id]
+  end
+
+  def has_room?
+    room_id = current_user.rooms.ids & @user.rooms.ids
+    redirect_to room_path(room_id[0]) if room_id.present?
   end
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,3 +1,17 @@
 class Room < ApplicationRecord
   has_many :messages
+  has_many :users_rooms
+  has_many :users, through: :users_rooms
+
+  def create_with_users_rooms(user1, user2)
+    ActiveRecord::Base.transaction do
+      users_rooms.build(user: user1)
+      users_rooms.build(user: user2)
+      save!
+    end
+    true
+  rescue => e
+    logger.warn e
+    false
+  end
 end
